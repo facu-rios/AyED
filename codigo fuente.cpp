86ea1f1dadf195144dd86450a655c67d5a810b6f
facundo rios occhiuzzi <friosocchiuzzi@frba.utn.edu.ar>
2023-06-01 17:44:19 -0300

    Add files via upload

---
+++ codigo fuente.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    double pi = 0.0;
+    double epsilon = 1e-6; // Precisión deseada
+    double term = 1.0;
+    int denominator = 1;
+    int sign = 1;
+
+    while (true) {
+        pi += sign * term;
+        denominator += 2;
+        term = 1.0 / denominator;
+        sign = -sign;
+
+        // Comprobar si alcanzamos la precisión deseada
+        if (4 * term < epsilon) {
+            break;
+        }
+    }
+
+    pi *= 4;
+
+    std::cout.precision(7);
+    std::cout << "Valor de pi: " << pi << std::endl;
+
+    return 0;
+}
+
